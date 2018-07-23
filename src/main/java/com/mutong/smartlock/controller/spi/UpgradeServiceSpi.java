9794c6e707492cdc982432ba01d74971697d9d15
shangchuanshun <519983770@qq.com>
2018-07-23 16:23:53 +0800

    在线更新

--- src/main/java/com/mutong/smartlock/controller/spi/UpgradeServiceSpi.java
+++ src/main/java/com/mutong/smartlock/controller/spi/UpgradeServiceSpi.java
@@ -1,26 +1,17 @@
 package com.mutong.smartlock.controller.spi;
 
-import com.mutong.smartlock.common.ErrorCode;
-import com.mutong.smartlock.common.LockException;
 import com.mutong.smartlock.common.Result;
 import com.mutong.smartlock.common.Upgrade;
-import com.mutong.smartlock.controller.AddUserService;
 import com.mutong.smartlock.controller.UpgradeService;
-import com.mutong.smartlock.controller.request.AddUserRequest;
-import com.mutong.smartlock.controller.response.AddUserResponse;
 import com.mutong.smartlock.controller.response.UpgradeResponse;
-import com.mutong.smartlock.service.spi.UserManageSpi;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
 import java.io.*;
 
 @RestController
